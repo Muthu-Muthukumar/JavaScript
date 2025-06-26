5bc6b8774e37e2d8e46c86b0f10783da79ee6a3e
Muthukumar <163752485+Muthu-Muthukumar@users.noreply.github.com>
2025-06-26 13:35:07 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,13 @@
+var Language = "java script";
+ 
+var isloggedin = false;
+
+var count = 67;
+let age = 34;
+
+var college;
+college = "XYZ";
+
+console.log(count);
+console.log("count");
+console.log(age);
---
+++ variables2.js
@@ -0,0 +1,16 @@
+const uid ="mk1234";
+
+var name = "MK";
+var email = "mk@gmail.com";
+var password = 56789;
+var confirmpassword = 56789;
+var count = 0;
+var isloggedinFromGoogle = false;
+
+//console.log("Name is:" + name);
+console.log(`
+    Unique id:${uid}
+    User Name:${name}
+    Email:${email}
+    Password:${password}
+    `);
